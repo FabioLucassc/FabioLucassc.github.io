4d6911dc19a0b2b048505710382127dc2c7b6477
Fabio Lucas <fabio-lucas1.000@hotmail.com>
2021-01-30 15:19:25 -0300

    testes parte Final

--- js/script.js
+++ js/script.js
@@ -3,3 +3,15 @@ function mostraAlerta(){
     alert('Implementação da função em Andamento!');
 
 }
+
+function Circlle(el){
+
+    $(el).circleProgress({fill: {color:'#ff5c5c'}})
+    .on('circle-animation-progress', function(event, progress, stepValue){
+
+        $(this).find('strong').text(String(stepValue.toFixed(2)).substr(2)+'%');
+
+    });
+};
+
+Circlle('.porcentagem');
